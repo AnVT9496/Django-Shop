b240da180a9f1271c9ca82e8a9bfd28a6ecef34e
AnVT <anvt96@gmail.com>
2021-07-17 18:53:43 +0700

    session

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from home.models import Setting
 from django.contrib import admin
 from home.models  import *
+from django.contrib.sessions.models import Session
 # Register your models here.
 class SettingsAdmin(admin.ModelAdmin):
     list_display = ['title','company', 'update_at','status']
@@ -11,7 +12,11 @@ class ContactMessageAdmin(admin.ModelAdmin):
     readonly_fields =('name','subject','email','message','ip')
     list_filter = ['status']
 
-
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+admin.site.register(Session, SessionAdmin)
 
 
 admin.site.register(Setting, SettingsAdmin)
