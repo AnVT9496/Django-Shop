87b0052d7f5fbcc9b37a6fbbca735c1bfd8864ec
AnVT <anvt96@gmail.com>
2021-08-09 20:47:40 +0700

    add soldout badge in cart, center form login, signup, fix icon show password

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,4 +173,4 @@ EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'anvt96@gmail.com'
-EMAIL_HOST_PASSWORD = 'maylaconga1'
+EMAIL_HOST_PASSWORD = 'vutienan123'
