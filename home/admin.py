f61177ba7a475db922830073f536aba639b3b726
AnVT <anvt96@gmail.com>
2021-09-14 21:21:07 +0700

    auto refresh cart

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from home.models import Setting
 from django.contrib import admin
 from home.models  import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 # Register your models here.
 class SettingsAdmin(admin.ModelAdmin):
@@ -19,6 +21,15 @@ class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
         return obj.get_decoded()
     list_display = ['session_key', '_session_data', 'expire_date']
+
+class UserAdmin(BaseUserAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Session, SessionAdmin)
 
 
